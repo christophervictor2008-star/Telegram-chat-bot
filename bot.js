623f948f4bd08a83b36212f9631fd7f73419fb19
christophervictor2008 <christophervictor2008@gmail.com>
2026-03-30 10:06:20 +0100

    add Dockerfile

--- bot.js
+++ bot.js
@@ -255,4 +255,4 @@ const http = require('http');
 http.createServer((req, res) => {
   res.write('Bot is running!');
   res.end();
-}).listen(process.env.PORT || 3000);
+}).listen(process.env.PORT || 7860);
